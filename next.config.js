5d6ab4cd083c54b2e1f331a865588c9d23316a95
unknown <you@example.com>
2025-06-23 23:49:24 +0700

    修复 Vercel 部署问题：忽略 .next 目录，移除 swcMinify 配置

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     domains: ['kynu88.com'],
     formats: ['image/webp', 'image/avif'],
